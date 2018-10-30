04988ccd182c3367cb1d233a3ea6490050eced90
Lance Strahan <lance.strahan@boeing.com>
2018-10-30 17:07:52 -0600

    moment-es6

--- src/app/misc/misc/misc.component.ts
+++ src/app/misc/misc/misc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ComponentFactoryResolver, ViewContainerRef, ViewChild, Type, AfterViewInit } from '@angular/core';
+import moment from 'moment-es6';
 import { MessageComponent } from '../message/message.component';
 import { ToolbarService } from 'src/app/library/toolbar.service';
 
@@ -16,7 +17,44 @@ export class MiscComponent implements OnInit, AfterViewInit {
     private toolBarService: ToolbarService) { }
 
   ngOnInit() {
-    
+    console.log('\n\n\n');
+
+    let jan1 = moment.utc('1900-01-01T00:00:00');
+
+    console.log(`jan1 initial value = ${jan1.utc().toISOString()}`);
+
+    let jan2 = jan1.utc().add(1, 'days');
+
+    console.log(`:\> jan2 = jan1.add('days', 1)`);
+    console.log(`jan1 = ${jan1.utc().toISOString()}`);
+    console.log(`jan2 = ${jan2.utc().toISOString()}`);
+
+    let jan3 = jan2.utc().add(1, 'days');
+
+    console.log(`:\> jan3 = jan2.add('days', 1)`);
+    console.log(`jan1 = ${jan1.utc().toISOString()}`);
+    console.log(`jan2 = ${jan2.utc().toISOString()}`);
+    console.log(`jan3 = ${jan3.utc().toISOString()}`);
+
+    console.log('\nusing clone()\n\n');
+
+    jan1 = moment.utc('1900-01-01T00:00:00');
+
+    console.log(`jan1 initial value = ${jan1.utc().toISOString()}`);
+
+    jan2 = jan1.utc().clone().add(1, 'days');
+
+    console.log(`:\> jan2 = jan1.clone().add('days', 1)`);
+    console.log(`jan1 = ${jan1.utc().toISOString()}`);
+    console.log(`jan2 = ${jan2.utc().toISOString()}`);
+
+    jan3 = jan2.utc().clone().add(1, 'days');
+
+    console.log(`:\> jan3 = jan2.clone().add('days', 1)`);
+    console.log(`jan1 = ${jan1.utc().toISOString()}`);
+    console.log(`jan2 = ${jan2.utc().toISOString()}`);
+    console.log(`jan3 = ${jan3.utc().toISOString()}`);
+    console.log('\n\n\n');
   }
 
   ngAfterViewInit(): void {

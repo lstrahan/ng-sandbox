1852eb43f33b0871c726a5e2732b7a504431e30e
Lance Strahan <lance.strahan@boeing.com>
2018-12-03 16:40:55 -0700

    stuff

--- projects/sandbox/src/app/material.module.ts
+++ projects/sandbox/src/app/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, MatOptionModule, MatGridListModule, MatProgressBarModule,
     MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDialogModule, MatSnackBarModule, MatSelectModule, MatInputModule,
     MatSidenavModule, MatCardModule, MatIconModule, MatRadioModule, MatProgressSpinnerModule, MatTabsModule, MatListModule,
-    MatButtonToggleModule, MatTooltipModule
+    MatButtonToggleModule, MatTooltipModule, MatExpansionModule
 } from '@angular/material';
 
 @NgModule({
@@ -10,13 +10,13 @@ import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, M
         MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, MatOptionModule, MatGridListModule, MatProgressBarModule,
         MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDialogModule, MatSnackBarModule, MatSelectModule, MatInputModule,
         MatSidenavModule, MatCardModule, MatIconModule, MatRadioModule, MatProgressSpinnerModule, MatTabsModule, MatListModule,
-        MatButtonToggleModule, MatTooltipModule
+        MatButtonToggleModule, MatTooltipModule, MatExpansionModule
     ],
     exports: [
         MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, MatOptionModule, MatGridListModule, MatProgressBarModule,
         MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDialogModule, MatSnackBarModule, MatSelectModule, MatInputModule,
         MatSidenavModule, MatCardModule, MatIconModule, MatRadioModule, MatProgressSpinnerModule, MatTabsModule, MatListModule,
-        MatButtonToggleModule, MatTooltipModule
+        MatButtonToggleModule, MatTooltipModule, MatExpansionModule
     ],
 })
 export class MaterialModule { }

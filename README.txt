7d24a664c5ea937e66fbab07201ae6d9c05e6ff0
Lance Strahan <lance.strahan@boeing.com>
2019-06-27 16:07:08 -0600

    remove library

--- README.txt
+++ README.txt
@@ -1,6 +1,1 @@
-run the following command to install the ags-hmi-library...
-
-    yarn add file:./ags-hmi-library
-
-
 node webSocketServer.js
--- projects/sandbox/src/app/misc/misc/misc.component.ts
+++ projects/sandbox/src/app/misc/misc/misc.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ComponentFactoryResolver, ViewContainerRef, ViewChild, Type } from '@angular/core';
 import { MessageComponent } from '../message/message.component';
-import { IconService } from 'ags-hmi-library';
+import { IconService } from '../../library/icon.service';
 
 // https://jaxenter.com/dynamically-create-component-angular-142720.html
 // https://netbasal.com/dynamically-creating-components-with-angular-a7346f4a982d
